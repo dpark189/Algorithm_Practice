b46c758a5ba4a75029edf55275417faa53d79880
Daniel Park <dpark189@gmail.com>
2018-09-04 20:54:54 -0400

    did i finish my sudoku from codewars

--- practice.js
+++ practice.js
@@ -274,4 +274,86 @@ function pal (str, i, j) {
   return right - left - 1;
 }
 
-console.log(longestPalindrome("cbbd"))
+console.log(longestPalindrome("cbbd"));
+
+// Did I finish my sudoku?
+//
+// Write a function done_or_not/DoneOrNot passing a board (list[list_lines]) as parameter. If the board is valid return 'Finished!', otherwise return 'Try again!'
+//
+// Sudoku rules:
+//
+// Complete the Sudoku puzzle so that each and every row, column, and region contains the numbers one through nine only once.
+//
+// There are 9 rows in a traditional Sudoku puzzle. Every row must contain the numbers 1, 2, 3, 4, 5, 6, 7, 8, and 9. There may not be any duplicate numbers in any row. In other words, there can not be any rows that are identical.
+//
+// In the illustration the numbers 5, 3, 1, and 2 are the "givens". They can not be changed. The remaining numbers in black are the numbers that you fill in to complete the row.
+//
+// There are 9 columns in a traditional Sudoku puzzle. Like the Sudoku rule for rows, every column must also contain the numbers 1, 2, 3, 4, 5, 6, 7, 8, and 9. Again, there may not be any duplicate numbers in any column. Each column will be unique as a result.
+//
+// In the illustration the numbers 7, 2, and 6 are the "givens". They can not be changed. You fill in the remaining numbers as shown in black to complete the column.
+//
+// A region is a 3x3 box like the one shown to the left. There are 9 regions in a traditional Sudoku puzzle.
+//
+// Like the Sudoku requirements for rows and columns, every region must also contain the numbers 1, 2, 3, 4, 5, 6, 7, 8, and 9. Duplicate numbers are not permitted in any region. Each region will differ from the other regions.
+//
+// In the illustration the numbers 1, 2, and 8 are the "givens". They can not be changed. Fill in the remaining numbers as shown in black to complete the region.
+
+function doneOrNot(board){
+  //your code here
+  if (!colCheck(board)) {return 'Try again!';};
+  if (!regions(board)) return 'Try again!';
+  for (let i = 0; i < board.length; i++){
+    if(!rowCheck(board[i])) return 'Try again!';
+  }
+  return 'Finished!';
+}
+
+function rowCheck(arr) {
+  const row = JSON.stringify(arr.sort());
+  const ref = JSON.stringify([1,2,3,4,5,6,7,8,9]);
+  return row === ref;
+}
+
+function transpose(board){
+  const len = board[0].length;
+  let tran = new Array(len);
+  for (let i = 0; i < len; i++){
+    tran[i] = new Array(len);
+  }
+  for(let i = 0; i < len; i++){
+    for (let j = 0; j < len; j++){
+      tran[j][i] = board[i][j];
+    }
+  }
+  return tran;
+}
+
+function colCheck(board){
+  const tran = transpose(board);
+  for(let i = 0; i < board.length; i++) {
+    if (!rowCheck(tran[i])) {
+        return false;
+    }
+  }
+  return true;
+}
+
+function regions(board) {
+  const len = board[0].length;
+  let reg = new Array(len);
+  for(let i =0; i < len; i++) {
+    reg[i] = [];
+  }
+  for(let i = 0; i < len; i++){
+    for(let j = 0; j < len; j++){
+      const rowGroup = Math.floor(i/3);
+      const colGroup = Math.floor(j/3) * 3;
+      const group = colGroup + rowGroup;
+      reg[group].push(board[i][j]);
+    }
+  }
+  for (let i = 0; i < len; i++){
+    if (!rowCheck(reg[i])) return false;
+  }
+  return true;
+}
